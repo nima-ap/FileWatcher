589abea29c3cbd1de169a0ad0ac97f49b7f5b45e
Nima Adibpour <65734148+nima-ap@users.noreply.github.com>
2024-04-11 11:50:10 -0700

    Initial commit

---
+++ FileWatcher/Person.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace FileWatcher
+{
+	public class Person
+    {
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+	}
+}
+
---
+++ FileWatcher/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using FileWatcher;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+
+string dir = Environment.CurrentDirectory;
+FileInfo file = new(Path.Combine(dir, "test.json"));
+if (!file.Exists)
+{
+    string json = JsonConvert.SerializeObject(new Person() { Name = "Jane Doe" });
+    File.WriteAllText(file.FullName, json);
+}
+
+PhysicalFileProvider fileProvider = new(dir);
+
+ChangeToken.OnChange(() => fileProvider.Watch("test.json"), () =>
+{
+    int maxRetries = 5;
+    int retryTimeoutMs = 50;
+
+    while (maxRetries > 0)
+    {
+        try
+        {
+            using Stream stream = file.OpenRead();
+            using StreamReader reader = new(
+                stream,
+                encoding: Encoding.UTF8,
+                detectEncodingFromByteOrderMarks: true,
+                bufferSize: 1024,
+                leaveOpen: false);
+
+            Person person = JsonConvert.DeserializeObject<Person>(reader.ReadToEnd());
+            Console.WriteLine($"Parsed person name: {person.Name}");
+            
+            maxRetries = 0;
+        }
+        catch (IOException)
+        {
+            maxRetries--;
+            if (maxRetries == 0)
+            {
+                throw;
+            }
+
+            Thread.Sleep(retryTimeoutMs);
+            retryTimeoutMs *= 2;
+        }
+    }
+});
+
+// To stall and prevent immediate exit
+Console.ReadLine();
